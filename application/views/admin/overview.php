7648ca2fb3a81100e9b5228126b020910a3dcca5
Alief Dwi Arjuna  Alief Dwi Arjuna <aliefarjuna1892@gmail.com>
2020-09-12 09:55:01 +0700

    second-commit

---
+++ application/views/admin/overview.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.php">
+
+    <title>Satgas.Covid</title>
+  </head>                                               
+<body>
+
+<nav class="navbar navbar-expand-md navbar-light bg-white sticky-top shadow">
+  <a class="navbar-brand" href="#"><img src="1.png"> alt="logo satgas.covid"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Informasi</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Hubungi Kami</a>
+      </li>
+      <li style="padding-top: 10px;">
+        <a href="javascript:void(0)" class="btn-search"><span class="fa fa-search"></span>
+      </a></li>
+    </ul> 
+  </div>
+</div>
+</nav>
+
+<div class="container" style="padding-bottom: 15px; padding-top: 60px;">
+  <div class="row">
+    <div class="image col-lg-6" style="right: 10px;">
+      <img src="img/layer2.png" alt="gambar mentoring" class="img-fluid">
+    </div>
+    <div class="desc col-lg-6">
+      <h4>Satgas Covid<br>Wonosobo</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>Nulla euismod erat lorem, vel ornare neque tincidunt sed.<br>Interdum</p>
+    <div class="but">
+      <a class="btn btn-primary btn-lg shadow-sm" href="#" role="button"></a>
+    </div>
+    </div>
+  </div>
+</div>
+
+<div class="my-5"></div>
+
+<div class="container relative">
+		<center>
+      <h2><span class="label label-primary">Pusat  Informasi  Covid-19  </span></h2>
+    </center>
+		<center><h2><span class="label label-primary">TANGGAL 12-09-2020  </span></h2></center>
+						<br>
+						<br>
+						<div class="row">
+							
+							<div class="col-lg-3 col-xs-6">
+								
+								<div class="small-box bg-yellow">
+									<div class="inner">
+										<center>
+										<h2>48,881</h2>
+										<p><h4>ODR</h4></p>
+										</center>
+									</div>
+									<div class="icon">
+										<i class="ion ion-bag"></i>
+									</div>
+									<a  class="small-box-footer">Data ODR<i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+							<!-- Column -->
+							<div class="col-lg-3 col-xs-6">
+								
+								<div class="small-box bg-blue">
+									<div class="inner">
+										<center>
+										<h2>3,261</h2>
+										<p><h4>SUSPEK</h4></p>
+										</center>
+									</div>
+									<div class="icon">
+										<i class="ion ion-stats-bars"></i>
+									</div>
+									<a class="small-box-footer">Data Suspek<i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+							<!-- ./col -->
+							<!-- ./col -->
+							<div class="col-lg-3 col-xs-6">
+								
+								<div class="small-box bg-green">
+									<div class="inner">
+										<center>
+										<h2>19</h2>
+										<p><h4>PROBABLE</h4></p>
+										</center>
+									</div>
+									<div class="icon">
+										<i class="ion ion-pie-graph"></i>
+									</div>
+									<a class="small-box-footer">Data Probable<i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-xs-6">
+								
+								<div class="small-box bg-red">
+									<div class="inner">
+										<center>
+										<h2>406</h2>
+										<p><h4>KONFIRMASI</h4></p>
+										</center>
+									</div>
+									<div class="icon">
+										<i class="ion ion-person-add"></i>
+									</div>
+									<a  class="small-box-footer">Data Konfirmasi<i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+							
+							<div class="col-md-3 col-sm-6 col-xs-12">
+								<div class="info-box">
+									<span class="info-box-icon bg-yellow"><h4><br>ODR</h4></i></span>
+									<div class="info-box-content">
+										</br>
+										<span ><b>3,261</b>&nbsp;&nbsp;&nbsp;&nbsp;Suspek</span></br>
+										<span ><b>19</b>&nbsp;&nbsp;&nbsp;&nbsp;Probable</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3 col-sm-6 col-xs-12">
+								<div class="info-box">
+									<span class="info-box-icon bg-blue"><h4><br>SUSPEK</h4></i></span>
+									<div class="info-box-content">
+										</br>
+										<span ><b>17</b>&nbsp;Dirawat</span></br>
+										<span ><b>111</b>&nbsp;Isolasi Mandiri</span></br>
+										<span ><b>3,114</b>&nbsp;Discarded</span></br>
+									
+									</div>
+								</div>
+							</div>
+						
+							<div class="clearfix visible-sm-block"></div>
+							<div class="col-md-3 col-sm-6 col-xs-12">
+								<div class="info-box">
+									<span class="info-box-icon bg-green"><h5><br>PROBABLE</h5></i></span>
+									
+									<div class="info-box-content">
+									    </br>
+									    
+										<span ><b>19</b>&nbsp;Probable</span></br>
+						
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3 col-sm-6 col-xs-12">
+								<div class="info-box">
+									<span class="info-box-icon bg-red"><h5><br>Konfirmasi</h5></i></span>
+									<div class="info-box-content">
+									
+										<span ><b>217</b>&nbsp;Dirawat</span></br>
+										<span ><b>184</b>&nbsp;Sembuh</span></br>
+										<span ><b>5</b>&nbsp;Meninggal</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						  
+								<div class="alert alert-warning">
+									<h4>ODR</h4>
+
+									<p style align="justify">Semua pelaku perjalanan dari wilayah terjangkit atau memiliki riwayat perjalanan dari daerah transmisi lokal .</p>
+								</div>
+								<div class="alert alert-primary">
+									<h4>KASUS SUSPEK</h4>
+									<p align="justify">Orang dengan ISPA da pada 14 hari terakhir sebelum timbul gejala memiliki riwayat perjalanan atau tinggal di wilayah yang melaporkan transmisi.</p>
+									
+									<p align="justify">Orang dengan salah satu gejala/tanda ISPA* dan pada 14 hari terakhir sebelum timbul gejala memiliki riwayat kontak dengan kasus konfirmasi/probable covid-19.</p>
+									
+									<p align="justify">Orang dengan ISPA berat/pneumonia yang membutuhkan perawatan di rumah sakit dan tidak ada penyebab lain berdasarkan gambaran klinis yang meyakinkan.</p>
+								</div>
+								<div class="alert alert-success">
+									<h4>KASUS PROBABLE</h4>
+									<p align="justify">Kasus suspek dengan ISPA Berat/ARDS/meninggal dengan gambaran klinis yang meyakinkan covid-19 dan belum ada hasil pemeriksaan laboratorium RT-PCR</p>
+								</div>
+								<div class="alert alert-danger">
+									<h4>KASUS KONFIRMASI</h4>
+									<p align="justify">Seseorang yang dinyatakan positif terinfeksi virus covid-19 yang dibuktikan dengan pemeriksaan laboratorium RT-PCR.</p>
+								</div>
+							
+
+					</div>
+				</section>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
