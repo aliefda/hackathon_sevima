0a87432b5d9cc45e6bef6e8115207854e804a373
Alief Dwi Arjuna  Alief Dwi Arjuna <aliefarjuna1892@gmail.com>
2020-09-12 13:53:27 +0700

    fifth-commit

--- application/views/admin/overview.php
+++ application/views/admin/overview.php
@@ -129,61 +129,81 @@
 
             <div class="my-5"></div>
 
-								<div class="alert alert-warning">
-									<h4>ODR</h4>
-									<p style="align:justify;">Semua pelaku perjalanan dari wilayah terjangkit atau memiliki riwayat perjalanan dari daerah transmisi lokal .</p>
-								</div>
-								<div class="alert alert-primary">
-									<h4>KASUS SUSPEK</h4>
-									<p align= 'justify'>Orang dengan ISPA da pada 14 hari terakhir sebelum timbul gejala memiliki riwayat perjalanan atau tinggal di wilayah yang melaporkan transmisi.</p>
+						<div class="alert alert-warning">
+							<h4>ODR</h4>
+								<p style="align:justify;">Semua pelaku perjalanan dari wilayah terjangkit atau memiliki riwayat perjalanan dari daerah transmisi lokal .</p>
+						</div>
+						<div class="alert alert-primary">
+							<h4>KASUS SUSPEK</h4>
+								<p align= 'justify'>Orang dengan ISPA da pada 14 hari terakhir sebelum timbul gejala memiliki riwayat perjalanan atau tinggal di wilayah yang melaporkan transmisi.</p>
 									
-									<p align="justify">Orang dengan salah satu gejala/tanda ISPA* dan pada 14 hari terakhir sebelum timbul gejala memiliki riwayat kontak dengan kasus konfirmasi/probable covid-19.</p>
+								<p align="justify">Orang dengan salah satu gejala/tanda ISPA* dan pada 14 hari terakhir sebelum timbul gejala memiliki riwayat kontak dengan kasus konfirmasi/probable covid-19.</p>
 									
-									<p align="justify">Orang dengan ISPA berat/pneumonia yang membutuhkan perawatan di rumah sakit dan tidak ada penyebab lain berdasarkan gambaran klinis yang meyakinkan.</p>
-								</div>
-								<div class="alert alert-success">
-									<h4>KASUS PROBABLE</h4>
-									<p align="justify">Kasus suspek dengan ISPA Berat/ARDS/meninggal dengan gambaran klinis yang meyakinkan covid-19 dan belum ada hasil pemeriksaan laboratorium RT-PCR</p>
-								</div>
-								<div class="alert alert-danger">
-									<h4>KASUS KONFIRMASI</h4>
-									<p align="justify">Seseorang yang dinyatakan positif terinfeksi virus covid-19 yang dibuktikan dengan pemeriksaan laboratorium RT-PCR.</p>
-                </div>
+								<p align="justify">Orang dengan ISPA berat/pneumonia yang membutuhkan perawatan di rumah sakit dan tidak ada penyebab lain berdasarkan gambaran klinis yang meyakinkan.</p>
+						</div>
+						<div class="alert alert-success">
+							<h4>KASUS PROBABLE</h4>
+								<p align="justify">Kasus suspek dengan ISPA Berat/ARDS/meninggal dengan gambaran klinis yang meyakinkan covid-19 dan belum ada hasil pemeriksaan laboratorium RT-PCR</p>
+						</div>
+						<div class="alert alert-danger">
+							<h4>KASUS KONFIRMASI</h4>
+								<p align="justify">Seseorang yang dinyatakan positif terinfeksi virus covid-19 yang dibuktikan dengan pemeriksaan laboratorium RT-PCR.</p>
+            </div>
           </div>
 
-<div class="container-fluid" style="background: linear-gradient(to right, #4FA5FD, #61AEFE); -webkit-clip-path: polygon(100% 0, 100% 93%, 0 100%, 0 7%);">
-  <div class="text-center" style="padding-top: 150px;">
-    <form class="search" style="	padding-left: 23px; font-size: 13px; color: #A9A9A9; width: 700px; height: 40px;border: 1px solid white; border-radius: 30px; outline: none; transition: .3s;"> 
-      <input type="text" placeholder="Telusuri info atau topik" class="img-fluid">
-    </form>
-  </div>
+          <div class="my-5"></div>
 
-  <div class="tpc text-center text-white">
+<div class="container-fluid" style="background: linear-gradient(to right, #4FA5FD, #61AEFE); -webkit-clip-path: polygon(100% 0, 100% 93%, 0 100%, 0 7%);">
+  <div class="tpc text-center text-white" style="padding-top: 150px;">
       <h1>Informasi Terkini</h1>
   </div>
   <div class="container" style="padding-bottom: 70px;">
     <center>
       <div style="width: 260px;display: inline-block; ">
         <div class="topic img-fluid text-white">
-          <a href="https://covid19.go.id/p/berita/infografis-covid-19-11-september-2020" id="isi"><img src="img/layer11.png" id="gambarlogo"><img src="img/rectangle3.png" id="bg2"><img src="img/layer 6.png" id="bg1"></a> 
+          <a href="https://covid19.go.id/p/berita/infografis-covid-19-11-september-2020" id="isi"><img src="https://covid19.go.id/storage/app/uploads/public/5f5/b45/3eb/5f5b453ebd278721072603.png" 
+          id="gambarlogo" 
+          style="position: relative; 
+          border-radius: 10px;
+          z-index: 1;	
+          width: 250px;
+          height: 150px;"></a> 
           <p>Informasi perkembangan covid<br>11 September 2020</p>
         </div>
       </div>
       <div style="width: 260px;display: inline-block; ">
         <div class="topic img-fluid text-white">
-          <a href="https://covid19.go.id/p/berita/jubir-covid-lima-minggu-terakhir-dki-zona-merah" id="isi"><img src="img/layer11.png" id="gambarlogo"><img src="img/rectangle3.png" id="bg2"><img src="img/layer 7.png" id="bg1"></a> 
+          <a href="https://covid19.go.id/p/berita/jubir-covid-lima-minggu-terakhir-dki-zona-merah" id="isi"><img src="https://covid19.go.id/storage/app/uploads/public/5f5/a9b/045/5f5a9b0455520722714238.jpeg"
+          id="gambarlogo" 
+          style="position: relative; 
+          border-radius: 10px;
+          z-index: 1;	
+          width: 250px;
+          height: 150px;"></a> 
           <p>Informasi perkembangan covid<br>10 September 2020</p>
         </div>
       </div>
       <div style="width: 260px;display: inline-block; ">
         <div class="topic img-fluid text-white">
-          <a href="https://covid19.go.id/p/berita/kasus-aktif-di-indonesia-masih-lebih-baik-dari-rata-rata-dunia" id="isi"><img src="img/layer11.png" id="gambarlogo"><img src="img/rectangle3.png" id="bg2"><img src="img/layer 8.png" id="bg1"></a> 
+          <a href="https://covid19.go.id/p/berita/kasus-aktif-di-indonesia-masih-lebih-baik-dari-rata-rata-dunia" id="isi"><img src="https://covid19.go.id/storage/app/uploads/public/5f5/a9b/3f3/5f5a9b3f34ebf697942252.jpeg"
+          id="gambarlogo" 
+          style="position: relative; 
+          border-radius: 10px;
+          z-index: 1;	
+          width: 250px;
+          height: 150px;"></a> 
           <p>Informasi perkembangan covid<br>10 September 2020</p>
         </div>
       </div>
       <div style="width: 260px;display: inline-block;margin-bottom: 100px; ">
         <div class="topic img-fluid text-white">
-          <a href="https://covid19.go.id/p/berita/standarisasi-harga-tes-pcr-sedang-dirumuskan" id="isi"><img src="img/layer11.png" id="gambarlogo"><img src="img/rectangle3.png" id="bg2"><img src="img/layer 9.png" id="bg1"></a> 
+          <a href="https://covid19.go.id/p/berita/standarisasi-harga-tes-pcr-sedang-dirumuskan" id="isi"><img src="https://covid19.go.id/storage/app/uploads/public/5f5/a9a/525/5f5a9a5259e9e939231645.jpeg"
+          id="gambarlogo" 
+          style="position: relative; 
+          border-radius: 10px;
+          z-index: 1;	
+          width: 250px;
+          height: 150px;"></a> 
           <p>Informasi perkembangan covid<br>10 September 2020</p>
         </div>
       </div>
@@ -191,8 +211,73 @@
   </div>
 </div>
 
+<section id="homepage" class="home page-section parallax-1 overlay-light-alpha-10" style="padding-top: 70px;">
+	<div class="table-content">
+		<div class="text-center">
+			<h2><span class="">Hubungi kami di Call Center </br></br></span></h2>
+				<h2><a href="tel:02863303121"><span class="btn btn-danger text-white">(0286) 3303121 (Call Center)</span></a></h2>
+				<h2><a href="whatsapp://send?text=Hello&phone=+628112600121"><span class="btn btn-danger text-white">08112600121 (Call Center)</span></a></h2>
+				<h2><a href="tel:081327234282"><span class="btn btn-warning text-white">081327234282 (dinkes)</span></a></h2>
+				<h2><a href="tel:082138117495"><span class="btn btn-warning text-white">082138117495 (dinkes)</span></a></h2>
+        <h2><a href="tel:081327291428"><span class="btn btn-warning text-white">081327291428 (dinkes)</span></a></h2>
+          </br></br>
+        <h2><span class="">Atau isi form dibawah ini</br></span></h2>
+		</div>
+	</div>
+</section>
 
+<div class="my-5"></div>
 
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+			<div class="blog-item clearfix">						
+				<div class="comments-heading text-center mb-30">
+					<hgroup>
+						<h2 class="font-face1 section-heading">Contact</h2>
+					</hgroup>									
+				</div>
+				<form method="post" action="https://corona.wonosobokab.go.id/contact/send" role="form" class="form">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								 <input type="text" name="name" class="full_width" placeholder="Name *" maxlength="100" required="">										
+						  </div>
+				    </div>
+
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text" name="hp" class="full_width" placeholder="Masukkan Nomor HP *" maxlength="100" required="">									
+							</div>
+						</div>
+					
+						<div class="col-md-12">
+							<div class="form-group">
+								<input type="email" name="email" class="full_width" placeholder="Email *" maxlength="100" required="">									
+							</div>
+						</div>
+                      
+            <div class="col-md-12">
+							<div class="form-group">
+								<input type="text" name="subject" class="full_width" placeholder="Judul / Subjek *" maxlength="100" required="">									
+							</div>
+						</div>
+                      
+            <div class="col-md-12">
+							<div class="form-group">
+								<textarea name="message" class="full_width" rows="6" placeholder="Isi PEsan *" maxlength="400" required></textarea>										
+							</div>
+						</div>
+            
+            <div class="col-md-12 center-xs">
+							<button type="submit" class="btn bg-black white-color">
+								Submit
+							</button>
+						</div>	
+						</div>		
+          </form>
+        </div>
+
+          
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
